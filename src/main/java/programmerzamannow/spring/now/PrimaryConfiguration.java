e4acf122b7889dbd5cf8ebad3d9186adb37a1afe
TOMI-PC\tomih <tomihartanto96@gmail.com>
2025-02-22 11:14:40 +0700

    Private Bean
    Signed-off-by: TOMI-PC\tomih <tomihartanto96@gmail.com>

---
+++ src/main/java/programmerzamannow/spring/now/PrimaryConfiguration.java
@@ -0,0 +1,21 @@
+package programmerzamannow.spring.now;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import programmerzamannow.spring.now.data.Foo;
+
+@Configuration
+public class PrimaryConfiguration {
+
+    @Primary
+    @Bean
+    public Foo foo1() {
+        return new Foo();
+    }
+
+    @Bean
+    public Foo foo2() {
+        return new Foo();
+    }
+}
